5563cc110f8ff6e023603fe82873b468b01e3d65
RJ Laguna <45117346+rjdlaguna@users.noreply.github.com>
2023-03-07 14:38:01 +0800

    first commit

---
+++ CodeFirstModel/Logs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstModel
+{
+    public class Logs
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ObjectId {get; set;}
+        public char Value { get; set; }
+        public DateTime ValueStamp { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedDate { get; set; }   
+        public DateTime ModifiedDate { get; set; }  
+
+    }
+}
